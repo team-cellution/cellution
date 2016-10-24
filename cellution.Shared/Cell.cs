be4d7e842f25d82c4d32a43a88e2ce97e2de6997
Yud07 <myudysky@gmail.com>
2016-10-24 19:58:04 -0400

    Eat A, C, G, T all implemented.
    The two cells eat forever.

--- cellution.Shared/Cell.cs
+++ cellution.Shared/Cell.cs
@@ -42,7 +42,7 @@ namespace cellution
                 velocity = Vector2.Zero;
                 behavior = -1;
             }
-            Console.WriteLine(name + " " + behavior);
+
             rand = World.Random.NextDouble();
             if (behavior == -1) // If the Cell is doing nothing.
             {
@@ -56,48 +56,24 @@ namespace cellution
                     }
                 }
             }
+            Console.WriteLine(name + " " + behavior);
             switch (behavior)
             {
                 // Eat A
                 case 0:
-                    //Console.WriteLine("Case 0");
-                    Vector2 newTarget = position;
-                    double nTDist = Double.MaxValue;
-                    foreach (Resource r in Game1.world.resourceManager.resources)
-                    {
-                        if (r.resourceType == Resource.ResourceTypes.A)
-                        {
-                            //Console.WriteLine(r.sprite.position);
-                            double temp = Vector2.Distance(r.sprite.position, position);
-                            //Console.WriteLine(temp + " vs " + nTDist);
-                            if (temp < nTDist)
-                            {
-                                nTDist = temp;
-                                newTarget = r.sprite.position;
-                            }
-                        }
-                    }
-                    //Console.WriteLine("newTarget" + newTarget);
-                    if (newTarget != position)
-                    {
-                        targetPosition = newTarget;
-                        velocity = new Vector2(targetPosition.X - position.X, targetPosition.Y - position.Y);
-                        velocity.Normalize();
-                        velocity *= 5.0f;
-                    }
-                    //Console.WriteLine("targetPosition" + targetPosition);
+                    eat(0);
                     break;
                 // Eat C
                 case 1:
-                    behavior = -1;
+                    eat(1);
                     break;
                 // Eat G
                 case 2:
-                    behavior = -1;
+                    eat(2);
                     break;
                 // Eat T
                 case 3:
-                    behavior = -1;
+                    eat(3);
                     break;
                 // Divide
                 case 4:
@@ -120,6 +96,56 @@ namespace cellution
                     break;
             }
             base.Update();
+        }
+        private void eat(int resourceType)
+        {
+            Resource.ResourceTypes rType;
+            if (resourceType == 0)
+            {
+                rType = Resource.ResourceTypes.A;
+            }
+            else if (resourceType == 1)
+            {
+                rType = Resource.ResourceTypes.C;
+            }
+            else if (resourceType == 2)
+            {
+                rType = Resource.ResourceTypes.G;
+            }
+            else if (resourceType == 3)
+            {
+                rType = Resource.ResourceTypes.T;
+            }
+            else
+            {
+                throw new System.ArgumentException("Parameter must be between 0 (A) and 3 (T)", "resourceType");
+            }
+            
+            Vector2 newTarget = position;
+            double nTDist = Double.MaxValue;
+            foreach (Resource r in Game1.world.resourceManager.resources)
+            {
+                if (r.resourceType == rType)
+                {
+                    double temp = Vector2.Distance(r.sprite.position, position);
+                    if (temp < nTDist)
+                    {
+                        nTDist = temp;
+                        newTarget = r.sprite.position;
+                    }
+                }
+            }
+            if (newTarget != position)
+            {
+                targetPosition = newTarget;
+                velocity = new Vector2(targetPosition.X - position.X, targetPosition.Y - position.Y);
+                velocity.Normalize();
+                velocity *= 5.0f;
+            }
+            else // If there are none of these resources
+            {
+                behavior = -1;
+            }
         }
     }
 }
